b682b87101f56e027b38f471436e8677c8e8768d
Michael Ye <michaelye956@gmail.com>
2023-09-20 05:12:48 +0000

    commented out csi stuff and added more logs to confirm if the task has been added and saved

--- agent/engine/docker_task_engine.go
+++ agent/engine/docker_task_engine.go
@@ -1144,32 +1144,33 @@ func (engine *DockerTaskEngine) AddTask(task *apitask.Task) {
 		return
 	}
 	if task.IsEBSTaskAttachEnabled() {
-		if csiTask, ok := engine.loadedDaemonTasks["ebs-csi-driver"]; ok {
-			logger.Info("engine ebs CSI driver is running", logger.Fields{
-				field.TaskID: csiTask.GetID(),
-			})
-		} else {
-			// TODO update 'ebs-csi-driver' as config param or const
-			var csiStartErr error
-			if ebsCsiDaemonManager, ok := engine.daemonManagers["ebs-csi-driver"]; ok {
-				csiTask, csiStartErr = ebsCsiDaemonManager.CreateDaemonTask()
-				engine.loadedDaemonTasks["ebs-csi-driver"] = csiTask
-				engine.AddTask(csiTask)
-				logger.Info("docker_task_engine: Added EBS CSI task to engine")
-			} else {
-				csiStartErr = errors.New("Unable to find ebs-csi-driver in engine")
-			}
-			if csiStartErr != nil {
-				logger.Error("Unable to start ebsCsiDaemon for task in the engine", logger.Fields{
-					field.TaskID: task.GetID(),
-					field.Error:  err,
-				})
-				task.SetKnownStatus(apitaskstatus.TaskStopped)
-				task.SetDesiredStatus(apitaskstatus.TaskStopped)
-				engine.emitTaskEvent(task, err.Error())
-				return
-			}
-		}
+		logger.Info("Task is EBSTaskAttachEnabled")
+		// if csiTask, ok := engine.loadedDaemonTasks["ebs-csi-driver"]; ok {
+		// 	logger.Info("engine ebs CSI driver is running", logger.Fields{
+		// 		field.TaskID: csiTask.GetID(),
+		// 	})
+		// } else {
+		// 	// TODO update 'ebs-csi-driver' as config param or const
+		// 	var csiStartErr error
+		// 	if ebsCsiDaemonManager, ok := engine.daemonManagers["ebs-csi-driver"]; ok {
+		// 		csiTask, csiStartErr = ebsCsiDaemonManager.CreateDaemonTask()
+		// 		engine.loadedDaemonTasks["ebs-csi-driver"] = csiTask
+		// 		engine.AddTask(csiTask)
+		// 		logger.Info("docker_task_engine: Added EBS CSI task to engine")
+		// 	} else {
+		// 		csiStartErr = errors.New("Unable to find ebs-csi-driver in engine")
+		// 	}
+		// 	if csiStartErr != nil {
+		// 		logger.Error("Unable to start ebsCsiDaemon for task in the engine", logger.Fields{
+		// 			field.TaskID: task.GetID(),
+		// 			field.Error:  err,
+		// 		})
+		// 		task.SetKnownStatus(apitaskstatus.TaskStopped)
+		// 		task.SetDesiredStatus(apitaskstatus.TaskStopped)
+		// 		engine.emitTaskEvent(task, csiStartErr.Error())
+		// 		return
+		// 	}
+		// }
 	}
 	// Check if ServiceConnect is Needed
 	if task.IsServiceConnectEnabled() {
@@ -1205,6 +1206,7 @@ func (engine *DockerTaskEngine) AddTask(task *apitask.Task) {
 		engine.state.AddTask(task)
 		if dependencygraph.ValidDependencies(task, engine.cfg) {
 			engine.startTask(task)
+			logger.Debug("task started")
 		} else {
 			logger.Error("Task has circular dependencies; unable to start", logger.Fields{
 				field.TaskID: task.GetID(),
@@ -1215,6 +1217,8 @@ func (engine *DockerTaskEngine) AddTask(task *apitask.Task) {
 			engine.emitTaskEvent(task, err.Error())
 		}
 		return
+	} else {
+		logger.Debug("task already exists")
 	}
 	// Update task
 	engine.updateTaskUnsafe(existingTask, task)
