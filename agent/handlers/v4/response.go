d0ebe749e2defd71e70338af6b83a89b7b28777b
mye956 <michaelye956@gmail.com>
2024-07-23 20:59:16 +0000

    fix static check

--- agent/handlers/v4/response.go
+++ agent/handlers/v4/response.go
@@ -60,7 +60,7 @@ func NewTaskResponse(
 			Networks:          networks,
 		})
 	}
-
+	task, _ := state.TaskByArn(taskARN)
 	return &tmdsv4.TaskResponse{
 		TaskResponse:      v2Resp,
 		Containers:        containers,
